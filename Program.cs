b1e652a3e823bb8e622f879ca1d25df00ce57e03
J-F Bilodeau <jfbilodeau@chronogears.com>
2025-12-05 12:00:20 -0500

    Remove 'List files' step from CI/CD workflow and fix app build method in Program.cs

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 
-var app = builder.Build();
+var app = builder.BuildX();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
